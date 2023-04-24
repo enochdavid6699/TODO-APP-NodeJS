d2d33abc8f7b2a2b3c594e2ad9f73f7c90ea138f
Enoch <enochdavid6699@gmail.com>
2023-04-24 14:49:21 +0530

    Ser up routers ,controllers ,ejs etc

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//Use Express Router
+app.use('/' , require('./routes/index'));
+
+app.set('view engine' , 'ejs'); 
+app.set('views' , './views');
+
 app.listen(port , function(err){
 
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+module.exports=router;
+
+const homeController = require('../controller/home_controller');
+router.get('/' , homeController.home);
+
+//For any further routes access from here
+//router.use('/router_name' , require('./router_file'));
