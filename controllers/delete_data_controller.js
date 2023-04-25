6537a83c51bf132111dc32ffab7cdcd247b6f527
Enoch <enochdavid6699@gmail.com>
2023-04-25 23:16:02 +0530

    Make the delete also async

--- controllers/delete_data_controller.js
+++ controllers/delete_data_controller.js
@@ -1,19 +1,18 @@
 //  delete data controller
 
 const Todo = require('../models/todo')
-module.exports.home_delete = async function(req,res){
 
-    try {
-        for(let id in req.body.ids){
-            let deletedTodo = await Todo.findByIdAndDelete( req.body.ids[id] , function (deletedTodo) {});
-        }    
-        return res.redirect('/');
-    } catch (error) {
-        console.log(error);
+module.exports.home_delete = async function(req, res) {
+  try {
+    for (let id in req.body.ids) {
+      await Todo.findByIdAndDelete(req.body.ids[id]);
     }
-    
-  
-}    
+    return res.redirect('/');
+  } catch (error) {
+    console.log(error);
+  }
+}
+ 
 
  // console.log(`This is the id received to delete: ${req.body}`)
 
