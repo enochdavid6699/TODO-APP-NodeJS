e9504037126df38961cd5d73e8665d1bb9ce4962
Enoch <enochdavid6699@gmail.com>
2023-04-26 14:00:53 +0530

    Semi-working delete button

--- controllers/delete_data_controller.js
+++ controllers/delete_data_controller.js
@@ -1,17 +1,19 @@
 //  delete data controller
 
-const Todo = require('../models/todo')
+const Todo = require('../models/todo');
 
 module.exports.home_delete = async function(req, res) {
   try {
-    for (let id in req.body.ids) {
-      await Todo.findByIdAndDelete(req.body.ids[id]);
+    const ids = req.body.ids; // get the array of ids from the request body
+    for (let id of ids) {
+      await Todo.findByIdAndDelete(id);
     }
     return res.redirect('/');
   } catch (error) {
     console.log(error);
   }
 }
+
  
 
  // console.log(`This is the id received to delete: ${req.body}`)
